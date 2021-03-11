87ea87e1ba678a7298ac1c1f392421243f0a586b
john <eeee>
2021-03-11 01:02:14 +0100

    [+] add moore information on game loby

--- src/client/src/Components/Pages/Game/GameMenu/GameMenu.jsx
+++ src/client/src/Components/Pages/Game/GameMenu/GameMenu.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Button from '@material-ui/core/Button';
 import HeaderBar from './../../../Organisms/HeaderBar/HeaderBar';
 
+import Typography from '@material-ui/core/Typography'
 
 import CenterPage from './../../../Atoms/Layout/CenterPage';//'./../components/Layout/CenterPage'
 
@@ -11,6 +12,12 @@ const GameMenu = ({ username, room, funcRunGame, funcLeaveRoom, owner}) => {
             <HeaderBar text={`${username} @ ${room?.name}`} variant={'h5'} />
             {owner &&
             <Button onClick={funcRunGame}>RUN GAME</Button>
+            }
+            <Typography variant='body1'>Player {room?.userList?.length} / 7</Typography>
+            {
+                room?.userList.length > 0 &&
+            room.userList.map((_username) =>
+                <Typography variant='body2'>{_username}</Typography>)
             }
             <Button onClick={funcLeaveRoom}>LEAVE ROOM</Button>
         </CenterPage>
